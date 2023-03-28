2e25c5b22c6d8dac262e786eac0e53a8445abd63
jordanlan <121735947+jordanlan@users.noreply.github.com>
2023-03-27 20:48:41 -0400

    Add files via upload

---
+++ API/login.php
@@ -0,0 +1,50 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+function login_user($username, $password){
+
+	$conn = new mysqli("localhost", "root", "26382523Pb", "contact_manager");
+
+	if($conn->connect_error){
+		echo("\nFailed to connect to mySQL server. \n");
+	}
+
+	else{
+
+		$stmt = $conn->prepare("Call log_on(?, ?)");
+
+		$stmt->bind_param("ss", $username, $password);
+
+		//$stmt->bind_param("ss", $inData["userName"], $inData["password"]);
+
+		$stmt->execute();
+
+		if(!$stmt){
+			echo("\nError executing query");
+			exit();
+		}
+		else{
+			$success = "User logged in\n";
+			echo($success);
+		}
+
+		$stmt->close();
+	}
+
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+	$username = $_POST['userName'];
+	$password = $_POST['password'];
+
+	$result = login_user($username, $password);
+
+	echo json_encode(array('success' => $result));	
+}
+
+?>
---
+++ API/signup-check.php
@@ -0,0 +1,47 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+function add_user($username, $password){
+	$firstname = '';
+	$lastname = '';
+
+	$conn = new mysqli("localhost", "root", "26382523Pb", "contact_manager");
+
+	if($conn->connect_error){
+		echo("\nFailed to connect to mySQL server. \n");
+	}
+
+	else{
+		$stmt = $conn->prepare("Call create_user(?, ?, ?, ?)");
+
+		$stmt->bind_param("ssss", $username, $password, $firstname, $lastname);
+
+		$stmt->execute();
+
+		if(!$stmt){
+			echo("\nError executing query");
+			exit();
+		}
+		else{
+			$success = "User created\n";
+			echo($success);
+		}
+
+		$stmt->close();
+	}
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+
+	$result = add_user($username, $password);
+
+	echo json_encode(array('success' => $result));	
+}
+
+?>
